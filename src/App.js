dfb4127e3b4ae0d3e4983ddc800162901172ea7c
rithikrice <rithikchopra2000@gmail.com>
2021-01-27 19:19:13 +0530

    Added my-carousel title-message scroll-down component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@
 
 //components
 import MyNavbar from "./components/my-navbar/mynavbar.component";
+import MyCarousal from "./components/my-carousal/my-carousal.component";
+import TitleMessage from './components/title-message/title-message.component'
+
 
 import './App.css';
 
@@ -9,7 +12,8 @@ const App=() => {
   return (
     <div>
       <MyNavbar />
-      My Portfolio
+      <MyCarousal />
+      <TitleMessage />>
     </div>
   );
 }
--- src/themes/theme.js
+++ src/themes/theme.js
@@ -2,5 +2,5 @@
 export const theme = 
 {
     primary : "#0b0c10",
-    textColor : "#66fcf1",
+    textColor : "#F0A08F",
 };
